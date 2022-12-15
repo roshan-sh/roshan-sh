10bee7fda82a020d3df7d80ba219ff5c1cb672c6
Roshan <rp207045@gmail.com>
2022-12-16 01:05:17 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const PORT = process.env.PORT || 5000;
+const router = require("./router");
+const app = express();
+
+const server = http.createServer(app);
+const io = socketio(server);
+app.use(router); // router to server for connection
+
+//using socket io connection for live response
+io.on("connection", (socket) => {
+    console.log("we have a new connection");
+    socket.on("disconnect", () => {
+        console.log("user had left the chat.");
+    });
+});
+
+server.listen(PORT, () => console.log(`server is runnig on PORT ${PORT}`));

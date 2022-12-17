73f42938e9af3935fad6cc25047df256a63309dc
Roshan <rp207045@gmail.com>
2022-12-17 12:12:36 +0530

    second commit

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 const router = require("./router");
 const app = express();
+const cors = require("cors");
 
 const server = http.createServer(app);
 const io = socketio(server);
 app.use(router); // router to server for connection
+app.use(cors());
 
 //using socket io connection for live response
 io.on("connection", (socket) => {
